1a18eafa9e2980e5ddbc35a5027a6c05c6d0eec2
alejandro <averduguezvidal@frba.utn.edu.ar>
2022-04-17 01:32:15 -0300

    hola

---
+++ eje1_6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int dividendo=2; 
+    int divisor=2;
+
+    printf("%d dividido %d es %d\n",dividendo, divisor, dividendo/divisor);
+    return 0;
+}
---
+++ eje1_7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int multiplicando=2; 
+    int multiplicador=2;
+
+    printf("%d x %d es %d\n",multiplicando, multiplicador, multiplicador*multiplicando);
+    return 0;
+}
